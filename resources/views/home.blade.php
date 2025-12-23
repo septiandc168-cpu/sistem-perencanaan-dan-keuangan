b3bc4a1132190cbbc960ff3dc696f2f494ffc46a
Septian <septiandc168@gmail.com>
2025-12-23 23:02:06 +0700

    mengganti template dengan adminlte

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,24 @@
-@extends('layouts.mantis')
+@extends('layouts.adminlte')
+@section('content_title', 'Home')
 
 @section('content')
-<div class="">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        Welcome to Aplikasi Perencanaan Kegiatan, <strong
+                            class="capitalize">{{ auth()->user()->name }}</strong>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
